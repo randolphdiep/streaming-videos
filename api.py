0c2ac8251f23934690f16c122fcdf5c5c382b6bb
phongdbt <phongdbt@runsystem.net>
2024-05-15 15:50:44 +0700

    feat: delete api.py file

--- api.py
+++ api.py
@@ -1,33 +1,101 @@
+from pathlib import Path
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import JSONResponse
-from fastapi import FastAPI, UploadFile, File,Response
-from starlette.responses import StreamingResponse
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Request, Response, Form, Depends, UploadFile, File
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse,RedirectResponse
+from fastapi import Header
+from fastapi.templating import Jinja2Templates
 import shutil
 import os
-import mimetypes
+import requests
+import json
+from pytube import YouTube
 from datetime import datetime
 
+
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/uploaded-images", StaticFiles(directory="uploaded-images"), name="uploaded-images")
+CHUNK_SIZE = 500*500  # 500KB
+# video_path = Path("./uploaded-videos/20240414124036.mp4")
 
-origins = [
-    "http://localhost:8000",  # Allow only this origin
-    "*",
-]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.get("/streaming/{file_id}")
+async def read_root(request: Request, file_id: str):
+    # Đọc dữ liệu từ file-info.json
+    with open("file-info.json", "r") as log_file:
+        data = json.load(log_file)
 
-@app.post("/upload")
-async def upload_video(file: UploadFile = File(...)):
+    # Tìm thông tin về video có file_id tương ứng
+    video_info = next((video for video in data if video["file_id"] == file_id), None)
+    if video_info is None:
+        raise HTTPException(status_code=404, detail="Video not found")
+
+    # Trả về template với thông tin về video và danh sách các video khác
+    return templates.TemplateResponse("streaming-form.html",
+                                      {"request": request, "file_id": file_id, "video_info": video_info, "video_list": data})
+
+
+
+@app.get("/", response_class=HTMLResponse)
+async def get_video_info(request: Request):
+    with open("file-info.json", "r") as log_file:
+        data = json.load(log_file)
+        if isinstance(data, dict):  # Check if data is a dictionary
+            data = [data]  # Convert data to a list containing one element
+        return templates.TemplateResponse('list-video-form.html', {"request": request, "video_info": data})
+
+
+
+@app.get("/video/{file_id}")
+async def video_endpoint(range: str = Header(None), file_id: str = None):
+    # read file file-info.json and find the file with the file_id and extract the extension
+    with open("file-info.json", "r") as log_file:
+        data = json.load(log_file)
+    for item in data:
+        if item["file_id"] == file_id:
+            video_path = Path(f"./uploaded-videos/{file_id}{item['ext']}")
+            break
+    else:
+        raise HTTPException(status_code=404, detail="File not found")
+    start, end = range.replace("bytes=", "").split("-")
+    start = int(start)
+    end = int(end) if end else start + CHUNK_SIZE
+    with open(video_path, "rb") as video:
+        video.seek(start)
+        data = video.read(end - start)
+        filesize = str(video_path.stat().st_size)
+        headers = {
+            'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
+            'Accept-Ranges': 'bytes'
+        }
+        return Response(data, status_code=206, headers=headers, media_type="video/mp4")
+    
+@app.get('/upload-video', response_class=HTMLResponse)
+def get_basic_form(request: Request):
+    return templates.TemplateResponse("upload-form.html", {"request": request})
+
+@app.post("/upload-video")
+async def upload_video(title: str = Form(...), file: UploadFile = File(...), image: UploadFile = File(...)):
     # Generate a unique ID for the file based on the current datetime
     file_id = datetime.now().strftime("%Y%m%d%H%M%S")
     _, ext = os.path.splitext(file.filename)
+
+    data = {"file_id": file_id, "title": title, "ext": ext}
+    # Read existing data
+    with open("file-info.json", "r") as log_file:
+        existing_data = json.load(log_file)
+
+    # Append new data
+    existing_data.append(data)
+
+    with open("file-info.json", "w") as log_file:
+        json.dump(existing_data, log_file)
+
+     # Check if the file is a video
+    if ext not in ['.mp4', '.webm']:  # Add or remove extensions as needed
+        raise HTTPException(status_code=400, detail="Invalid file type. Please upload a video file.")
 
     file_name = f"{file_id}{ext}"
     # Define the path for the video
@@ -37,46 +105,48 @@ async def upload_video(file: UploadFile = File(...)):
     with open(video_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    # Append the original filename to a shared txt file
-    with open("file-names.txt", "a") as log_file:
-        log_file.write(f"{file_name}\n")
-    
-    # Return a success response
-    return JSONResponse(content={"message": "Video uploaded successfully"}, status_code=200)
+    # Save the image file
+    _, image_ext = os.path.splitext(image.filename)
+    image_name = f"{file_id}{image_ext}"
+    image_path = f"./uploaded-images/{image_name}"
+    with open(image_path, "wb") as buffer:
+        shutil.copyfileobj(image.file, buffer)
 
-@app.get("/stream-video/{file_id}")
-async def stream_video(file_id: str):
-    log_file_path = "file-names.txt"
-    video_directory = "./uploaded-videos/"
-    try:
-        with open(log_file_path, "r") as log_file:
-            for line in log_file:
-                current_file = line.split(".")
-                current_file_name = current_file[0]
-                if current_file_name == file_id:
-                    video_path = os.path.join(video_directory, line).strip()
-                    if os.path.exists(video_path):
-                        mime_type, _ = mimetypes.guess_type(line.strip())
-                        file_like = open(video_path, mode="rb")
-                        return StreamingResponse(file_like, media_type=mime_type)
-        raise HTTPException(status_code=404, detail="File ID not found in log")
-    except FileNotFoundError:
-        raise HTTPException(status_code=404, detail="Log file not found")
-    except Exception as e:
-        return Response(status_code=500, content=str(e))
-    
-@app.get("/files")
-async def list_files():
-    log_file_path = "file-names.txt"
-    files_list = []
-    try:
-        with open(log_file_path, "r") as file:
-            for line in file:
-                clean_line = line.strip()
-                if clean_line:
-                    files_list.append(clean_line)
-    except FileNotFoundError:
-        return JSONResponse(content={"error": "Log file not found"}, status_code=404)
-    except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=500)
-    return JSONResponse(files_list)
+    return RedirectResponse("/", status_code=303)
+
+@app.get("/download-video",response_class=HTMLResponse)
+async def download_youtube_video(request: Request):
+    return templates.TemplateResponse("download-form.html", {"request": request})
+
+@app.post("/download-video")
+async def download_youtube_video(video_url: str = Form(...)):
+    yt = YouTube(video_url)
+    stream = yt.streams.get_highest_resolution()
+
+    # Get current date and time
+    file_id = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Get the file extension
+    _, file_extension = os.path.splitext(stream.default_filename)
+
+    # Download and rename the video, store it in the ./uploaded-videos folder
+    stream.download(output_path='./uploaded-videos', filename=f"{file_id}{file_extension}")
+
+    # Get the URL of the thumbnail
+    thumbnail_url = yt.thumbnail_url
+
+    # Download the thumbnail and store it in the ./uploaded-images folder
+    response = requests.get(thumbnail_url)
+    with open(os.path.join('./uploaded-images', f'{file_id}.jpg'), 'wb') as out_file:
+        out_file.write(response.content)
+    data = {"file_id": file_id, "title": yt.title, "ext": file_extension}
+    # Read existing data
+    with open("file-info.json", "r") as log_file:
+        existing_data = json.load(log_file)
+     # Append new data
+    existing_data.append(data)
+
+    with open("file-info.json", "w") as log_file:
+        json.dump(existing_data, log_file)
+    return RedirectResponse("/", status_code=303)
+
